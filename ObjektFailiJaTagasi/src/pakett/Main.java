fb67a791f13d24bd166953ce1355f637cc020c0f
pele <pele@pealik.Asus>
2016-07-02 02:52:08 +0300

    INI Graafika loomine tõstetud eraldi Classi: INI_EditoriGraafika

--- ObjektFailiJaTagasi/src/pakett/Main.java
+++ ObjektFailiJaTagasi/src/pakett/Main.java
@@ -13,11 +13,10 @@ import javax.swing.JPanel;
 
 
 public class Main {
-	  final static boolean shouldFill = true;
-	    final static boolean shouldWeightX = true;
-	    final static boolean RIGHT_TO_LEFT = false;
+
 
 	public static void main(String[] args) throws Exception {
+		INI_EditoriGraafika.INI_EditoriKuvamine();
 		
 		String tekstiFail=""+GLOBAL.ProgramRunLocation+GLOBAL.INI_FailiNimi;
 		GLOBAL.INI_FailiStringideJada[0]="<0>{Pealkiri}";
@@ -68,103 +67,19 @@ public class Main {
 		}
 		
 		
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+
 		
 		
 		
 		
 	}
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("GridBagLayoutDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //Set up the content pane.
-        addComponentsToPane(frame.getContentPane());
- 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-    public static void addComponentsToPane(Container pane) {
-        if (RIGHT_TO_LEFT) {
-            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        }
- 
-        JButton button;
-    pane.setLayout(new GridBagLayout());
-    GridBagConstraints c = new GridBagConstraints();
-    if (shouldFill) {
-    //natural height, maximum width
-    c.fill = GridBagConstraints.HORIZONTAL;
-    }
- 
-    button = new JButton("Button 1");
-    if (shouldWeightX) {
-    c.weightx = 0.5;
-    }
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.gridx = 0;
-    c.gridy = 0;
-    pane.add(button, c);
- 
-    button = new JButton("Button 2");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.5;
-    c.gridx = 1;
-    c.gridy = 0;
-    pane.add(button, c);
- 
-    button = new JButton("Button 3");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.5;
-    c.gridx = 2;
-    c.gridy = 0;
-    pane.add(button, c);
- 
-    button = new JButton("Long-Named Button 4");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.ipady = 40;      //make this component tall
-    
-    c.weightx = 0.0;
-    c.gridwidth = 3;
-    c.gridx = 0;
-    c.gridy = 1;
-    pane.add(button, c);
- 
-    button = new JButton("5");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.ipady = 0;       //reset to default
-    c.weighty = 1.0;   //request any extra vertical space
-    c.anchor = GridBagConstraints.PAGE_END; //bottom of space
-    c.insets = new Insets(10,0,0,0);  //top padding
-    c.gridx = 1;       //aligned with button 2
-    c.gridwidth = 2;   //2 columns wide
-    c.gridy = 2;       //third row
-    pane.add(button, c);
-    }
+
  
 	
-	public static void INI_EditoriKuvamine(){
-		GridLayout experimentLayout = new GridLayout(0,2);
-			    								
-	JFrame PeaRaam = new JFrame("Raami Nimi");
-	PeaRaam.setBounds (100, 100, 300, 300);
-	PeaRaam.show();
 	
-	double size[][] =
-        {{0.25, 0.25, 0.25, 0.25},
-         {50, 40, 40, 40}};
-	PeaRaam.setLayout(experimentLayout);
-	JPanel PeaPaneel = new JPanel();
-	PeaRaam.add(PeaPaneel);
-	PeaRaam.setVisible(true);
-	PeaRaam.pack();
-	
+		
+		
+		
 	
 	
 	}
@@ -174,4 +89,4 @@ public class Main {
 	
 	
 
-}
+
